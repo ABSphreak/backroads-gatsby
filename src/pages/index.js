4efe987f316e32eb2e224eb888ac1e93504a1428
ABSphreak <absphreak@outlook.com>
2020-06-25 00:29:21 +0530

    ➕ Adding Styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,7 @@ const home = () => {
     <>
       {/* Using children prop in Layout to render */}
       <Layout>
-        <div>
-          <h1>Home Page</h1>
-          <Link to="/blog/">Blog</Link>
-        </div>
+        <div>BACKROADS</div>
       </Layout>
     </>
   )
