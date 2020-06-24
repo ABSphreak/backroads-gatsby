7cc0ea118621d2656ade7043eb596e3b1594c90c
ABSphreak <absphreak@outlook.com>
2020-06-25 01:23:20 +0530

    ➕ Footer Styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,41 @@
 import React from "react"
+import { Link } from "gatsby"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import { FaRProject } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()}.
+        All rights reserved.
+      </div>
+    </footer>
   )
 }
 
