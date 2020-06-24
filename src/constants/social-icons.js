81d5f06e9301838ce5c73432a03dc4b96da5bae3
ABSphreak <absphreak@outlook.com>
2020-06-25 00:46:23 +0530

    ➕ Adding more CSS

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitter, FaGithub } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https://facebook.com/originalphreak",
+  },
+  {
+    icon: <FaTwitter />,
+    url: "https://twitter.com/ABSphreak",
+  },
+  {
+    icon: <FaGithub />,
+    url: "https://github.com/ABSphreak",
+  },
+]
