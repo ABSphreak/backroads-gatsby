7d30b86ac4d382462b61172e47ca085d0411744f
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-06-25 15:44:23 +0530

    ➕ Site Metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,11 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine and say yes to adventure!",
+    author: "Abhinav Sharma",
+  },
   plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sass`],
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,9 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <div className={styles.navCenter}>
         <div className={styles.navHeader}>
-          <img src={logo} alt="BackRoads Logo" />
+          <Link to="/">
+            <img src={logo} alt="BackRoads Logo" />
+          </Link>
           <button type="button" className={styles.logoBtn} onClick={toggleNav}>
             <FaAlignRight className={styles.logoIcon} />
           </button>
