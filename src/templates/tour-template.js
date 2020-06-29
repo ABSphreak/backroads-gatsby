cc3a5bed8c1db361ea5389ed563fb7615a866e0b
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-06-30 00:23:07 +0530

    ➕ Added Content Toggles

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -6,6 +6,7 @@ import styles from "../css/template.module.css"
 import Img from "gatsby-image"
 import { FaMoneyBillWave, FaMap } from "react-icons/fa"
 import Day from "../components/SingleTour/Day"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Template = ({ data }) => {
   const {
@@ -50,8 +51,17 @@ const Template = ({ data }) => {
             </p>
           </div>
           <h4>starts on: {start}</h4>
-          <h4>duration: {days}</h4>
-          <p></p>
+          <h4>duration: {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>daily schedule</h2>
+          <div className={styles.journey}>
+            {journey.map((item, index) => {
+              return <Day key={index} day={item.day} info={item.info} />
+            })}
+          </div>
+          <AniLink fade to="/tours" className="btn-primary">
+            back to tours
+          </AniLink>
         </div>
       </section>
     </Layout>
