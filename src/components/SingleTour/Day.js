6d854616f4dc09f45fe7e65475becee3d631fa78
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-06-29 23:50:44 +0530

    ➕ Tour Detail Components

---
+++ src/components/SingleTour/Day.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Day = () => {
+  return <div>This is Day 1</div>
+}
+
+export default Day
--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,7 +1,85 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import styles from "../css/template.module.css"
+import Img from "gatsby-image"
+import { FaMoneyBillWave, FaMap } from "react-icons/fa"
+import Day from "../components/SingleTour/Day"
 
-const Template = () => {
-  return <div>hello from dummy template</div>
+const Template = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    start,
+    description: { description },
+    images,
+    journey,
+  } = data.tour
+
+  const [mainImage, ...tourImages] = images
+
+  return (
+    <Layout>
+      <StyledHero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {tourImages.map((item, index) => {
+              return (
+                <Img
+                  key={index}
+                  fluid={item.fluid}
+                  alt="single tour"
+                  className={styles.image}
+                />
+              )
+            })}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              starting from → ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>starts on: {start}</h4>
+          <h4>duration: {days}</h4>
+          <p></p>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd, MMMM Do YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
 
 export default Template
