c428a87aac6f2e9928c9d7775ac834b6accc3e9d
ABSphreak <absphreak@outlook.com>
2020-07-06 00:19:38 +0530

    ➕ Adding Pagination

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import styles from "../css/blog.module.css"
+import BlogCard from "../components/Blog/BlogCard"
+import Title from "../components/Title"
+
+const Blog = props => {
+  const { data } = props
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.id} blog={node} />
+          })}
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Blog
